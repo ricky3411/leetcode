066e941f30c9fc22e47edfa6685417539073b740
ricky3411 <78570667+ricky3411@users.noreply.github.com>
2024-08-19 01:17:26 -0400

    LC-two-sum [Runtime: 59 ms; Memory: 17.7 MB]

--- array/two-sum.py
+++ array/two-sum.py
@@ -1,14 +1,12 @@
 class Solution:
     def twoSum(self, nums: List[int], target: int) -> List[int]:
 
-        mapping = {}
+        map = {}
 
-        for idx, val in enumerate(nums):
-
+        for key, val in enumerate(nums):
             diff = target - val
-
-            if diff in mapping:
-                return [idx, mapping[diff]]
+            if diff in map:
+                return [map[diff], key]
             else:
-                mapping[val] = idx
+                map[val] = key
 
