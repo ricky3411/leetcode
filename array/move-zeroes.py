2254233947740891cf4445882666f322755add49
ricky3411 <78570667+ricky3411@users.noreply.github.com>
2024-08-19 00:30:34 -0400

    LC-move-zeroes [Runtime: 121 ms; Memory: 18.2 MB]

--- array/move-zeroes.py
+++ array/move-zeroes.py
@@ -6,7 +6,7 @@ class Solution:
 
         last = 0
 
-        for i in range(1,len(nums)):
+        for i in range(len(nums)):
             if nums[i] != 0:
                 temp = nums[last]
                 nums[last] = nums[i]
