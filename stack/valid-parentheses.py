21bd22fc15ce617d6218258f74776f52be1e12ad
ricky3411 <78570667+ricky3411@users.noreply.github.com>
2024-08-28 02:20:19 -0400

    LC-valid-parentheses [Runtime: N/A; Memory: N/A]

--- stack/valid-parentheses.py
+++ stack/valid-parentheses.py
@@ -1,36 +1,25 @@
 class Solution:
     def isValid(self, s: str) -> bool:
 
-        if len(s) == 1:
-            return False
+        if len(s) == 0:
+            return True
 
-        closeToOpen = {')':'(', ']':'[', '}':'{'}
+        map = {"}":"{", "]":"[", ")":"("}
         stack = []
 
-        for c in s:
-            if c in closeToOpen:
-                if stack  and stack[-1] == closeToOpen[c]:
-                    stack.pop()
+
+        for i in s:
+            if i in '([{':
+                stack.append(i)
+                print(stack)
+            
+            else:
+                pop = stack.pop()
+            
+                if map[i] == pop:
+                    continue
                 else:
                     return False
-            else:
-                stack.append(c)
         
         if len(stack) == 0:
             return True
-        else:
-            return False
-
-
-
-        
-
-
-
-
-
-
-
-        
-
-        
