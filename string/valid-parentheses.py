85c20d306951ef536c63bae19a6d59178233999d
ricky3411 <78570667+ricky3411@users.noreply.github.com>
2025-01-07 02:58:29 -0500

    LC-valid-parentheses [Runtime: N/A; Memory: N/A]

--- string/valid-parentheses.py
+++ string/valid-parentheses.py
@@ -15,11 +15,11 @@ class Solution:
         l = int(len(s)/2) -1
         r = int((len(s)-1)/2) +1
 
-        if s[r] == map[s[l]]:
-            l-=1
-            r+=1
-        else:
-            return False 
-        
-        return True
+        if l >= 0 and r <= len(s):
+            if s[l] in map and s[r] == map[s[l]]:
+                l-=1
+                r+=1
+            else:
+                return False 
             
+        return True
