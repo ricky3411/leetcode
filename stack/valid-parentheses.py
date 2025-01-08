b3a032c282a8b0c9889def58c3bdfbfc5ad965f4
ricky3411 <78570667+ricky3411@users.noreply.github.com>
2025-01-08 01:56:07 -0500

    LC-valid-parentheses [Runtime: 0 ms; Memory: 17.9 MB]

--- stack/valid-parentheses.py
+++ stack/valid-parentheses.py
@@ -1,28 +1,23 @@
 class Solution:
     def isValid(self, s: str) -> bool:
 
-        if len(s) == 0:
-            return True
+        if len(s) % 2 != 0:
+            return False
 
-        map = {"}":"{", "]":"[", ")":"("}
+        map = {"(":")", "[":"]", "{":"}"}
         stack = []
 
-
-        for i in s:
-            if i in '([{':
-                stack.append(i)
+        for c in s:
+            if c in map:
+                stack.append(c)
             
-            #closing bracket
             else:
-
-                #checking if the stack is empty, if it is return false because it should not be since 
-                #I just encountered a closing bracket that needs to match up with an opening one
-                if not stack:
-                    return False
-
-                pop = stack.pop()
-            
-                if map[i] != pop:
+                #closing bracket
+                if stack and c == map[stack[-1]]:
+                    stack.pop()
+                else:
                     return False
         
-        return len(stack) == 0
+        return len(stack) == 0 
+                
+        
