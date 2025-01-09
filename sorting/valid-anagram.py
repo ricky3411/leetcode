aa11a3bd94a0699a2b3a063e3501707e7b38efbf
ricky3411 <78570667+ricky3411@users.noreply.github.com>
2025-01-09 02:46:02 -0500

    LC-valid-anagram [Runtime: N/A; Memory: N/A]

--- sorting/valid-anagram.py
+++ sorting/valid-anagram.py
@@ -1,21 +1,5 @@
 class Solution:
     def isAnagram(self, s: str, t: str) -> bool:
 
-        count = [0] * 26
+        return set(s) == set(t)
         
-        # Count the frequency of characters in string s
-        for x in s:
-            count[ord(x) - ord('a')] += 1
-        
-        print(count)
-        
-        # Decrement the frequency of characters in string t
-        for x in t:
-            count[ord(x) - ord('a')] -= 1
-        
-        # Check if any character has non-zero frequency
-        for val in count:
-            if val != 0:
-                return False
-        
-        return True
