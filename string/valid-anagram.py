02142ad75a5869a85c794d72636fddc67df9a12c
ricky3411 <78570667+ricky3411@users.noreply.github.com>
2025-01-09 02:49:00 -0500

    LC-valid-anagram [Runtime: 11 ms; Memory: 17.7 MB]

--- string/valid-anagram.py
+++ string/valid-anagram.py
@@ -1,5 +1,22 @@
 class Solution:
     def isAnagram(self, s: str, t: str) -> bool:
 
-        return set(s) == set(t)
+        mapS = {}
+        mapT = {}
+
+        for i in s:
+            if i in mapS:
+                mapS[i] += 1
+            else:
+                mapS[i] = 1
+        
+        for i in t:
+            if i in mapT:
+                mapT[i] += 1
+            else:
+                mapT[i] = 1
+
+        return mapS == mapT
+        
+
         
