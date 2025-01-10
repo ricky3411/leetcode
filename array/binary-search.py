0c64ed7995e7c5782973741a92fd183701679706
ricky3411 <78570667+ricky3411@users.noreply.github.com>
2025-01-09 20:25:49 -0500

    LC-binary-search [Runtime: 10 ms; Memory: 18.6 MB]

--- array/binary-search.py
+++ array/binary-search.py
@@ -1,8 +1,18 @@
 class Solution:
     def search(self, nums: List[int], target: int) -> int:
 
-        for i in range(len(nums)):
-            if nums[i] == target:
-                return i
+        l, r = 0, len(nums)-1
+
+        while l <= r:
+            m = (l+r) // 2
+
+            if nums[m] < target:
+                l+=1 
+
+            elif nums[m] > target:
+                r-=1
+            
+            else:
+                return m
+            
         return -1
-        
